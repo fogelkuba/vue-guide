5bdce00a5d594244e1efd81bb430a481004c3b11
fogelkuba <fogelkuba@gmail.com>
2018-02-04 13:39:35 +0100

    40

--- applications/starbase-router/src/components/Item.vue
+++ applications/starbase-router/src/components/Item.vue
@@ -1,11 +1,12 @@
 <template>
-  <div class="col-md-4">
+  <div class="col-md-4" @click="switchItem">
       <div class="item-card">
           <div class="card-block">
               <h3 class="card-title">{{item.name}}</h3>
               <div v-for="(value, key, index) in item">
-                  <div class="item" v-if="index < 5">
-                      <strong class="item-details">{{key}}</strong>: <span class="item-details">{{value}}</span>
+                  <div class="item" v-if="index < 6">
+                      <strong class="item-details">
+                          {{key}}</strong>: <span class="item-details">{{value}}</span>
                   </div>
               </div>
           </div>
@@ -16,6 +17,22 @@
 
 <script>
 export default {
-    props: ['item']
+    props: ['passedItem', 'type'],
+    data(){
+        return {
+            item: {}
+        }
+    },
+    methods: {
+        switchItem(){
+            let randId = Math.floor(Math.random() * 62) + 1;
+            fetch(`https://swapi.co/api/${this.type}/${randId}`, {method: "GET"})
+            .then( response => response.json() )
+            .then( json => this.item = json )
+        }
+    },
+    created(){
+        this.item = this.passedItem;
+    }
 }
 </script>
