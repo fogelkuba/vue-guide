00dd61d0c85933dc7df64c850118dcb5c86b7b72
fogelkuba <fogelkuba@gmail.com>
2018-02-06 20:04:15 +0100

    49

--- applications/jokester/src/components/App.vue
+++ applications/jokester/src/components/App.vue
@@ -3,7 +3,17 @@
         <h3>Got Jokes?</h3>
         <button class="btn btn-primary" @click="initJokes">Get 10 random jokes</button>
         <div class="">
-            {{$store.state}}
+            <br>
+            <br>
+        	<div class="col-md-12">
+        	      <div class="row">
+                      <Joke v-for="(joke, index) in $store.state.jokes"
+                      :joke="joke"
+                      :key="index"
+                      />
+                  </div>
+        	</div>
+
         </div>
 
     </div>
@@ -11,10 +21,14 @@
 
 <script>
 import { mapActions } from 'vuex';
+import Joke from './Joke.vue';
 
 export default {
     methods: mapActions([
         'initJokes'
-    ])
+    ]),
+    components: {
+        Joke
+    }
 }
 </script>
