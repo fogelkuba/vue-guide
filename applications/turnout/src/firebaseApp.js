ea5f0779298371e4830c71d3600d2b45a4af6685
fogelkuba <fogelkuba@gmail.com>
2018-02-07 20:30:33 +0100

    57

---
+++ applications/turnout/src/firebaseApp.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase';
+
+const config = {
+  apiKey: "AIzaSyD6wbvnpA1NJyeCJiynrTpJdlLMUGLgt_I",
+  authDomain: "turnout-39b13.firebaseapp.com",
+  databaseURL: "https://turnout-39b13.firebaseio.com",
+  projectId: "turnout-39b13",
+  storageBucket: "",
+  messagingSenderId: "191172565806"
+};
+
+export const firebaseApp = firebase.initializeApp(config);
--- applications/turnout/src/main.js
+++ applications/turnout/src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import App from './components/App.vue';
 import VueRouter from 'vue-router'
+import { firebaseApp } from './firebaseApp';
 
 Vue.use(VueRouter);
 
@@ -20,6 +21,14 @@ const router = new VueRouter({
         }
     ]
 })
+
+firebaseApp.auth().onAuthStateChanged( user => {
+    if(user){
+        router.push('/dashboard')
+    }else{
+        router.replace('/signin')
+    }
+});
 
 new Vue({
     el: '#app',
