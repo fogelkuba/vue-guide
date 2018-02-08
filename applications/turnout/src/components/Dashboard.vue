79d5855b072cf5ef577cb641e730d4db59640578
fogelkuba <fogelkuba@gmail.com>
2018-02-08 20:18:38 +0100

    60

--- applications/turnout/src/components/Dashboard.vue
+++ applications/turnout/src/components/Dashboard.vue
@@ -1,6 +1,19 @@
 <template>
   <div class="">
-      Dashboard
+      <h3>Events Dashboard</h3>
+      <button class="btn btn-danger btn-small signout" @click="signOut">Sign out</button>
       {{$store.state}}
   </div>
 </template>
+
+<script>
+    import {firebaseApp} from '../firebaseApp';
+    export default {
+        methods: {
+            signOut(){
+                this.$store.dispatch('signOut');
+                firebaseApp.auth().signOut();
+            }
+        }
+    }
+</script>
